d0cbe7e867b9c0d5bd23f1313a578c5a839a4bcd
Anton <bazhan.anton@ukr.net>
2020-07-07 13:26:16 +0300

    Updates

---
+++ precache-manifest.3984cd8e29e43524d13b28263b0c674d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "173f3a44348a3c8b549f",
+    "url": "/react-quiz/static/css/main.6154ce91.chunk.css"
+  },
+  {
+    "revision": "173f3a44348a3c8b549f",
+    "url": "/react-quiz/static/js/main.173f3a44.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/react-quiz/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "af45168ecde8330867c4",
+    "url": "/react-quiz/static/js/runtime~main.af45168e.js"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/react-quiz/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/react-quiz/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "576d7af51d13fde23e5196d5e87954ac",
+    "url": "/react-quiz/index.html"
+  }
+];
